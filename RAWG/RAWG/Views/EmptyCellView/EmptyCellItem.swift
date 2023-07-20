6e3c7fbf9b5e3cc87577f7125ac2e58e07e950a1
Emir Keles <burhanemirkeles@outlook.com>
2023-07-20 14:50:07 +0300

    Add Empty Cell View

---
+++ RAWG/RAWG/Views/EmptyCellView/EmptyCellItem.swift
@@ -0,0 +1,31 @@
+//
+//  EmptyCellItem.swift
+//  RAWG
+//
+//  Created by Emir Keleş on 20.07.2023.
+//
+
+import Foundation
+import Carbon
+
+struct EmptyCellItem : IdentifiableComponent {
+
+  var id = 1
+  let referenceSizeHeight: CGFloat = 136
+
+  // MARK: - Component
+  func render(in content: EmptyCellView) { }
+
+  func referenceSize(in bounds: CGRect) -> CGSize? {
+    return CGSize(width: bounds.width, height: referenceSizeHeight)
+  }
+
+  func renderContent() -> EmptyCellView {
+    return EmptyCellView()
+  }
+
+  func shouldContentUpdate(with next: EmptyCellItem) -> Bool {
+    return false
+  }
+  
+}
---
+++ RAWG/RAWG/Views/EmptyCellView/EmptyCellView.swift
@@ -0,0 +1,56 @@
+//
+//  EmptyCellView.swift
+//  RAWG
+//
+//  Created by Emir Keleş on 20.07.2023.
+//
+
+import Foundation
+import SnapKit
+import Carbon
+
+final class EmptyCellView: UIView {
+
+  var emptyLabel: UILabel = UILabel()
+
+  private struct Const {
+    static let emptyLabelText: String = "No game has been searched.."
+    static let emptyLabelFont = UIFont(name: "Roboto-Bold", size: 18)
+    static let emptyLabelMinScaleFactor: CGFloat = 0.5
+
+    static let emptyLabelTopOffset = 38
+  }
+
+  override init(frame: CGRect){
+    super.init(frame: frame)
+    setupViews()
+    setupConstraints()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  func setupViews () {
+    self.addSubview(emptyLabel)
+
+    emptyLabel.textColor = .black
+    emptyLabel.textAlignment = .center
+    emptyLabel.numberOfLines = .zero
+    emptyLabel.lineBreakMode = .byWordWrapping
+    emptyLabel.textColor = .black
+    emptyLabel.adjustsFontSizeToFitWidth = true
+    emptyLabel.minimumScaleFactor = Const.emptyLabelMinScaleFactor
+    emptyLabel.baselineAdjustment = .alignCenters
+    emptyLabel.text = Const.emptyLabelText
+    emptyLabel.font = Const.emptyLabelFont
+  }
+
+  func setupConstraints() {
+    emptyLabel.snp.makeConstraints{
+      $0.top.equalToSuperview().offset(Const.emptyLabelTopOffset)
+      $0.centerX.equalToSuperview()
+    }
+
+  }
+}
