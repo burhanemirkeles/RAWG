3fd004d150c2a6c6495ac57e635aefb40b8d22d9
Emir Keles <burhanemirkeles@outlook.com>
2023-07-23 03:56:28 +0300

    Change favorites scene

--- RAWG/RAWG/Scenes/Favorites/FavoritesViewController.swift
+++ RAWG/RAWG/Scenes/Favorites/FavoritesViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Carbon
 
 class FavoritesViewController: UIViewController {
 
@@ -16,7 +17,32 @@ class FavoritesViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     view.addSubview(favoritesView)
+    configureFavoritesView(favoritesView)
+
+    configureTableView()
 
   }
+
+  private func configureFavoritesView(_ view: UIView) {
+
+    favoritesView.snp.makeConstraints {
+      $0.top.equalTo(0)
+      $0.leading.trailing.equalTo(0)
+      $0.height.equalTo(favoritesView.snp.height)
+    }
+  }
+
+  private func configureTableView() {
+    view.addSubview(tableView)
+    tableView.snp.makeConstraints {
+      $0.top.equalTo(favoritesView.snp.bottom)
+      $0.leading.equalTo(0)
+      $0.trailing.equalTo(0)
+      $0.bottom.equalToSuperview().offset(-82)
+    }
+    tableView.separatorStyle = .none
+    tableView.contentInset = UIEdgeInsets(top: -20, left: 0, bottom: 0, right: 0)
+  }
+
 }
 
